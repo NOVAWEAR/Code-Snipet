b8360e22044fddda6acbdfcca3c314eb1f1f2991
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-09 22:06:04 +0200

    Add V21 Brand Authority Commerce

---
+++ novawear-signature-demo/v21/v16.js
@@ -0,0 +1,2 @@
+document.write('<script src="../v16/v16.js"><\/script><script src="../v17/v17.js"><\/script><script src="../v18/v18.js"><\/script><script src="../v18/v18-polish.js"><\/script><script src="../v20/v20.js"><\/script><script src="v21.js"><\/script>');
+document.title=document.title.replace('V16','V21').replace('V17','V21').replace('V18','V21').replace('V19','V21').replace('V20','V21');
