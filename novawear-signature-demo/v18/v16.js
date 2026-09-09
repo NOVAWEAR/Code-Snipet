7bb79564cf3841fcfa893316f2ecb2e06d43c1cd
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-09 21:27:33 +0200

    Load V18 final polish layer

--- novawear-signature-demo/v18/v16.js
+++ novawear-signature-demo/v18/v16.js
@@ -1,2 +1,2 @@
-document.write('<script src="../v16/v16.js"><\/script><script src="../v17/v17.js"><\/script><script src="v18.js"><\/script>');
+document.write('<script src="../v16/v16.js"><\/script><script src="../v17/v17.js"><\/script><script src="v18.js"><\/script><script src="v18-polish.js"><\/script>');
 document.title=document.title.replace('V16','V18').replace('V17','V18');
