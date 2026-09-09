44f7ddbde669e0cb7ff6c1d75fe4c1fd7b84b05c
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-09 21:51:14 +0200

    Build V20 Luxury Interactive Commerce

---
+++ novawear-signature-demo/v20/v16.js
@@ -0,0 +1,2 @@
+document.write('<script src="../v16/v16.js"><\/script><script src="../v17/v17.js"><\/script><script src="../v18/v18.js"><\/script><script src="../v18/v18-polish.js"><\/script><script src="v20.js"><\/script>');
+document.title=document.title.replace('V16','V20').replace('V17','V20').replace('V18','V20').replace('V19','V20');
