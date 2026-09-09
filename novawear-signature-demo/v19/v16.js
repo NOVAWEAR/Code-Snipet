2ca077bcdaf122298b09bfd5b4640f112d667d6f
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-09 21:39:54 +0200

    Fix V19 script chain paths

--- novawear-signature-demo/v19/v16.js
+++ novawear-signature-demo/v19/v16.js
@@ -1,2 +1,2 @@
-document.write('<script src="../v18/v16.js"><\/script><script src="v19.js"><\/script>');
+document.write('<script src="../v16/v16.js"><\/script><script src="../v17/v17.js"><\/script><script src="../v18/v18.js"><\/script><script src="../v18/v18-polish.js"><\/script><script src="v19.js"><\/script>');
 document.title=document.title.replace('V18','V19').replace('V17','V19').replace('V16','V19');
