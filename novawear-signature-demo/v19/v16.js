52859cb85ccffe0bac6d52da5d0bee460b3165c0
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-09 21:35:35 +0200

    Wire V19 interactive layer to V18 commerce system

---
+++ novawear-signature-demo/v19/v16.js
@@ -0,0 +1,2 @@
+document.write('<script src="../v18/v16.js"><\/script><script src="v19.js"><\/script>');
+document.title=document.title.replace('V18','V19').replace('V17','V19').replace('V16','V19');
