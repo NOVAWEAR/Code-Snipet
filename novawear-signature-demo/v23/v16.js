f7452680959e4dc2761915498199d437dcfebac0
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-10 22:57:01 +0200

    Build V23 conversion architecture

---
+++ novawear-signature-demo/v23/v16.js
@@ -0,0 +1,2 @@
+document.write('<script src="../v22/v16.js"><\/script><script src="v23.js"><\/script>');
+document.title=document.title.replace('V22','V23').replace('V21','V23').replace('V20','V23').replace('V18','V23').replace('V16','V23');
