e2e953842f582ea2db895b566ee26378a3cd73ab
NOVAWEAR <167215822+NOVAWEAR@users.noreply.github.com>
2026-09-10 23:02:41 +0200

    Add V24 script loader

---
+++ novawear-signature-demo/v24/v16.js
@@ -0,0 +1,2 @@
+document.write('<script src="../v23/v16.js"><\/script><script src="v24.js"><\/script>');
+document.title=document.title.replace('V23','V24').replace('V22','V24').replace('V21','V24').replace('V20','V24').replace('V18','V24').replace('V16','V24');
